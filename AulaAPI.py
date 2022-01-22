288a87387f2a3da7fd1741da8a6ca109875f61f5
zzz <75046739+werp420@users.noreply.github.com>
2022-01-23 00:00:45 +0100

    Update AulaAPI.py

--- AulaAPI.py
+++ AulaAPI.py
@@ -6,17 +6,19 @@
  | |_) | | | | | (_| | |  | |_| || (__| | |_| | |_) |
  |____/|_|_| |_|\__,_|_|   \__, (_)___|_|\__,_|_.__/ 
                             __/ |                    
-                           |___/                      
+                           |___/             
+
 ~ Binary club
 We are publishing this code for educational purposes only and to circumvent Kombit A/S Breaking multiple-laws, GDPR and other privacy regulations.
 We are not responsible for any misuse of this code.
+
 This Issue has been reported to the Kombit A/S team but no response has been received, Therefor we demand that you do not use this code for any illegal purposes.
 Exposing Children SSN Numbers: (Social Security Number) or Better known as CPR numbers in Denmark.
 Exposing Phone Numbers
 Exposing Email Addresses
 Exposing Home Addresses
 ______________________________________________________________________________________________________________
-Project by Wrec & H4xton ~ The Binary.club team is a collective of Danish researches and hackers aged between 15-18 Years old. 
+Project by Wrec & H4xton ~ The Binary.club team is a collective of Danish researches and hackers aged between 15-19 Years old. 
 """
 
 import requests
